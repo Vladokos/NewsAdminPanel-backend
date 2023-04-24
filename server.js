586d4cbefbec19c022618896ab52264c01022cf5
Vlad <66181660+Vladokos@users.noreply.github.com>
2023-04-24 17:48:41 +0400

    fix

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.static("uploads"));
 app.use(express.static(path.join(__dirname, "build")));
 
-router.get("*", req, (res) => {
+router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
