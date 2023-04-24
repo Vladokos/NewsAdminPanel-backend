4440edead34bcaeecbb88d37681b85eb2d2eaa49
Vlad <66181660+Vladokos@users.noreply.github.com>
2023-04-24 17:23:18 +0400

    asd

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ app.use(express.static("uploads"));
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", function (req, res, next) {
+  console.log('first')
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.get("/", function (req, res, next) {
+  console.log('second')
   return res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
